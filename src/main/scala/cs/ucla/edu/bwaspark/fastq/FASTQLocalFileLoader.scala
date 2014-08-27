961e59746b19a1a57c1199084ae2a24e3dd4c291
Yu-Ting Chen <heeros@gmail.com>
2014-08-27 01:16:25 -0700

    pair-end RDD Loading

--- src/main/scala/cs/ucla/edu/bwaspark/fastq/FASTQLocalFileLoader.scala
+++ src/main/scala/cs/ucla/edu/bwaspark/fastq/FASTQLocalFileLoader.scala
@@ -124,5 +124,143 @@ class FASTQLocalFileLoader(batchedLineNum: Int) {
       }
    }
 
+
+   /**
+     *  Read the FASTQ files (pair-end, 2 files) from a local directory 
+     *  This function reads only a batch of FASTQs and should be called several times to read the whole FASTQ files
+     *  
+     *  @param reader1 the Java BufferedReader object to read a file line by line (on one end of the read)
+     *  @param reader2 the Java BufferedReader object to read a file line by line (on the other end of the read)
+     *  @param sc the spark context
+     *  @param batchedNum the number of lines to read per batch
+     */
+   def batchedPairEndRDDReader(reader1: BufferedReader, reader2: BufferedReader, sc: SparkContext, batchedNum: Int): RDD[(Null, SerializablePairEndFASTQRecord)] = {
+      val charset = Charset.forName("ASCII")
+      val encoder = charset.newEncoder()
+      var records = List[PairEndFASTQRecord]()
+      var lineNum = 0      
+      
+
+      breakable {
+         while(lineNum < batchedNum) {
+            var line = reader1.readLine()
+            if(line != null) {
+               val lineFields = line.split(" ")
+               var pairEndRecord = new PairEndFASTQRecord        
+       
+               if(lineFields.length == 1) {
+                  val name = encoder.encode( CharBuffer.wrap(lineFields(0)) ); 
+                  val seqString = reader1.readLine()
+                  val seqLength = seqString.size
+                  val seq = encoder.encode( CharBuffer.wrap(seqString) )
+                  // read out the third line
+                  reader1.readLine()
+                  val quality = encoder.encode( CharBuffer.wrap(reader1.readLine()) )
+                  val record = new FASTQRecord(name, seq, quality, seqLength, encoder.encode( CharBuffer.wrap("") ))
+                  pairEndRecord.setSeq1(new SerializableFASTQRecord(record))
+               } else if(lineFields.length == 2) {
+                  val name = encoder.encode( CharBuffer.wrap(lineFields(0)) );
+                  val comment = encoder.encode( CharBuffer.wrap(lineFields(1)) );
+                  val seqString = reader1.readLine()
+                  val seqLength = seqString.size
+                  val seq = encoder.encode( CharBuffer.wrap(seqString) )
+                  // read out the third line
+                  reader1.readLine()
+                  val quality = encoder.encode( CharBuffer.wrap(reader1.readLine()) )
+                  val record = new FASTQRecord(name, seq, quality, seqLength, comment)
+                  pairEndRecord.setSeq1(new SerializableFASTQRecord(record))
+               }
+               else
+                  println("Error: Input format not handled")
+
+               line = reader2.readLine
+
+               if(line == null) println("Error: the number of two FASTQ files are different")
+               else {
+                 
+                  val lineFields = line.split(" ")
+                
+                  if(lineFields.length == 1) {
+                     val name = encoder.encode( CharBuffer.wrap(lineFields(0)) ); 
+                     val seqString = reader2.readLine()
+                     val seqLength = seqString.size
+                     val seq = encoder.encode( CharBuffer.wrap(seqString) )
+                     // read out the third line
+                     reader2.readLine()
+                     val quality = encoder.encode( CharBuffer.wrap(reader2.readLine()) )
+                     val record = new FASTQRecord(name, seq, quality, seqLength, encoder.encode( CharBuffer.wrap("") ))
+                     pairEndRecord.setSeq2(new SerializableFASTQRecord(record))
+                  } else if(lineFields.length == 2) {
+                     val name = encoder.encode( CharBuffer.wrap(lineFields(0)) );
+                     val comment = encoder.encode( CharBuffer.wrap(lineFields(1)) );
+                     val seqString = reader2.readLine()
+                     val seqLength = seqString.size
+                     val seq = encoder.encode( CharBuffer.wrap(seqString) )
+                     // read out the third line
+                     reader2.readLine()
+                     val quality = encoder.encode( CharBuffer.wrap(reader2.readLine()) )
+                     val record = new FASTQRecord(name, seq, quality, seqLength, comment)
+                     pairEndRecord.setSeq2(new SerializableFASTQRecord(record))
+                  }
+                  else
+                     println("Error: Input format not handled")
+               }
+ 
+               records = pairEndRecord :: records
+               lineNum += 8
+            }
+            else {
+               isEOF = true
+               break;
+            }
+         }
+      }
+
+      val serializedRecords = records.map(new SerializablePairEndFASTQRecord(_))
+      val rdd = sc.parallelize(serializedRecords)
+      val pairRDD = rdd.map(rec => (null, rec))
+      pairRDD
+   }   
+
+
+   /**
+     *  Read the FASTQ file from the local file system and store it in HDFS
+     *  The FASTQ is encoded and compressed in the Parquet+Avro format in HDFS 
+     *
+     *  @param sc the spark context
+     *  @param inFile1 the input FASTQ file in the local file system (on one end of the read)
+     *  @param inFile2 the input FASTQ file in the local file system (on the other end of the read)
+     *  @param outFileRootPath the root path of the output FASTQ files in HDFS. 
+     *    Note that there will be several directories since the local large FASTQ file is read and stored in HDFS with several batches
+     */
+   def storePairEndFASTQInHDFS(sc: SparkContext, inFile1: String, inFile2: String, outFileRootPath: String) {
+      val reader1 = new BufferedReader(new FileReader(inFile1))
+      val reader2 = new BufferedReader(new FileReader(inFile2))
+
+      val parquetHadoopLogger = Logger.getLogger("parquet.hadoop")
+      parquetHadoopLogger.setLevel(Level.SEVERE)
+
+      var i: Int = 0
+
+      while(!isEOF) {
+         val pairRDD = batchedPairEndRDDReader(reader1, reader2, sc, batchedLineNum)
+         val job = new Job(pairRDD.context.hadoopConfiguration)
+
+         // Configure the ParquetOutputFormat to use Avro as the serialization format
+         //ParquetOutputFormat.setCompression(job, CompressionCodecName.GZIP)
+         ParquetOutputFormat.setCompression(job, CompressionCodecName.UNCOMPRESSED)
+         ParquetOutputFormat.setEnableDictionary(job, true)
+         ParquetOutputFormat.setBlockSize(job, 128 * 1024 * 1024)
+         ParquetOutputFormat.setPageSize(job, 1 * 1024 * 1024)
+      
+         // Pass the Avro Schema
+         AvroParquetOutputFormat.setSchema(job, cs.ucla.edu.avro.fastq.PairEndFASTQRecord.SCHEMA$)
+         // Save the RDD to a Parquet file in our temporary output directory
+         val outputPath = outFileRootPath + "/"  + i.toString();
+         pairRDD.saveAsNewAPIHadoopFile(outputPath, classOf[Void], classOf[PairEndFASTQRecord], classOf[AvroParquetOutputFormat], ContextUtil.getConfiguration(job))
+         i += 1
+      }
+   }
+
 }
 
---
+++ src/main/scala/cs/ucla/edu/bwaspark/fastq/SerializablePairEndFASTQRecord.java
@@ -0,0 +1,48 @@
+package cs.ucla.edu.bwaspark.fastq;
+
+import cs.ucla.edu.avro.fastq.*;
+import org.apache.avro.io.*;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.IOException;
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+/**
+ *   For now, Spark does not support Avro. This class is just a quick
+ *   workaround that (de)serializes PairEndFASTQRecord objects using Avro.
+ */
+public class SerializablePairEndFASTQRecord extends PairEndFASTQRecord implements Serializable {
+
+    private void setValues(PairEndFASTQRecord rec) {
+        setSeq1(rec.getSeq1());
+        setSeq2(rec.getSeq2());
+    }
+
+    public SerializablePairEndFASTQRecord(PairEndFASTQRecord rec) {
+        setValues(rec);
+    }
+
+    private void writeObject(java.io.ObjectOutputStream out)
+            throws IOException {
+        DatumWriter<PairEndFASTQRecord> writer = new SpecificDatumWriter<PairEndFASTQRecord>(PairEndFASTQRecord.class);
+        Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+        writer.write(this, encoder);
+        encoder.flush();
+    }
+
+    private void readObject(java.io.ObjectInputStream in)
+            throws IOException, ClassNotFoundException {
+        DatumReader<PairEndFASTQRecord> reader =
+                new SpecificDatumReader<PairEndFASTQRecord>(PairEndFASTQRecord.class);
+        Decoder decoder = DecoderFactory.get().binaryDecoder(in, null);
+        setValues(reader.read(null, decoder));
+    }
+
+    private void readObjectNoData()
+            throws ObjectStreamException {
+    }
+
+}
+
